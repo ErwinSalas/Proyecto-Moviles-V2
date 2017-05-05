ee807b2b64eae1bf3519df8dad09ad64ccfca01d
ErwinSalas <erwinsalas42@gmail.com>
2017-05-04 23:02:56 -0600

    subida de archivos

---
+++ app/src/main/java/com/example/pavilion/proyectomoviles/OpcvCamaraV.java
@@ -0,0 +1,84 @@
+package com.example.pavilion.proyectomoviles;
+
+
+import java.io.FileOutputStream;
+import java.util.List;
+import org.opencv.android.JavaCameraView;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.Size;
+import android.util.AttributeSet;
+import android.util.Log;
+
+public class OpcvCamaraV extends JavaCameraView implements PictureCallback {
+
+    private static final String TAG = "Sample::OpcvCamaraV";
+    private String mPictureFileName;
+
+    public OpcvCamaraV(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public List<String> getEffectList() {
+        return mCamera.getParameters().getSupportedColorEffects();
+    }
+
+    public boolean isEffectSupported() {
+        return (mCamera.getParameters().getColorEffect() != null);
+    }
+
+    public String getEffect() {
+        return mCamera.getParameters().getColorEffect();
+    }
+
+    public void setEffect(String effect) {
+        Camera.Parameters params = mCamera.getParameters();
+        params.setColorEffect(effect);
+        mCamera.setParameters(params);
+    }
+
+    public List<Size> getResolutionList() {
+        return mCamera.getParameters().getSupportedPreviewSizes();
+    }
+
+    public void setResolution(Size resolution) {
+        disconnectCamera();
+        mMaxHeight = resolution.height;
+        mMaxWidth = resolution.width;
+        connectCamera(getWidth(), getHeight());
+    }
+
+    public Size getResolution() {
+        return mCamera.getParameters().getPreviewSize();
+    }
+
+    public void takePicture(final String fileName) {
+        Log.i(TAG, "Taking picture");
+        this.mPictureFileName = fileName;
+
+        mCamera.setPreviewCallback(null);
+
+        mCamera.takePicture(null, null, this);
+    }
+
+    @Override
+    public void onPictureTaken(byte[] data, Camera camera) {
+        Log.i(TAG, "Saving a bitmap to file");
+
+        mCamera.startPreview();
+        mCamera.setPreviewCallback(this);
+
+        try {
+            FileOutputStream fos = new FileOutputStream(mPictureFileName);
+
+            fos.write(data);
+            fos.close();
+
+        } catch (java.io.IOException e) {
+            Log.e("PictureDemo", "Exception in photoCallback", e);
+        }
+
+    }
+}
